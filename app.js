7639c0d3c2a1f718b8ae911b70c5c309d36c07ef
Herop <thesecon@gmail.com>
2017-03-09 17:47:40 +0900

    Apply mongo lab (#3)
    * DataBase(mLab) 적용 중...
    
    * 필요없는 파일 정리

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var glob = require('glob');
+var mongoose = require('mongoose');
 
 app.set('views', './app/views');
 app.set('view engine', 'ejs');
@@ -15,4 +16,11 @@ controllers.forEach(function (controller) {
 var port = process.env.PORT || 5000;
 app.listen(port, function () {
     console.log('Express server listening on port ' + port);
+});
+
+mongoose.connect('mongodb://herop:herop1216@ds153659.mlab.com:53659/db_herop');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('mongolab connect!');
 });
